292beba23fe3b12cf928ca851941ecac5a4c5811
Starciad <davigamerlsf16@gmail.com>
2026-01-18 16:42:44 -0300

    feat: update enemy values.

--- projects/games/battle/src/enemy.c
+++ projects/games/battle/src/enemy.c
@@ -5,11 +5,11 @@ entity enemy_create(int challenge_level) {
     const int health = random_int(1, 5) + challenge_level * 2;
 
     return (entity) {
-        .coins = (challenge_level * 10) + random_int(5, 15),
+        .coins = (challenge_level * 2) + random_int(1, 5),
         .base_attack = challenge_level,
-        .critical_chance = challenge_level + random_int(5, 10),
+        .critical_chance = challenge_level + random_int(1, 5),
         .current_health = health,
-        .defense = challenge_level + random_int(0, 1),
+        .defense = challenge_level - 1,
         .maximum_health = health,
         .is_defending = false,
         .is_preparing_attack = false,
